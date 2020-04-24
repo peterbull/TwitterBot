555ed4d7c6c50c42f7ec244122c3b1736a7d8b5a
peter <petercbull@gmail.com>
2020-04-24 09:20:36 -0400

    iterate and follow back specific follower

--- tweettweet.py
+++ tweettweet.py
@@ -1,11 +1,25 @@
 import tweepy
 import config
+import time
 
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
 auth.set_access_token(config.access_token, config.token_secret)
 
 api = tweepy.API(auth)
+user = api.me()
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+
+def limit_handler(cursor):
+    try:
+        while True:
+            yield cursor.next()
+            break
+    except tweepy.RateLimitError:
+        time.sleep(1000)
+
+
+# Generous Bot
+for follower in limit_handler(tweepy.Cursor(api.followers).items()):
+    if follower.name == 'Smapdi':
+        follower.follow()
+        break
